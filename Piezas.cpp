1e80217044a67390b0079f67c79d069f9612cb38
s-b-m <amajor2@mail.csuchico.edu>
2019-12-13 22:21:02 -0800

    fixing constructor

--- Piezas.cpp
+++ Piezas.cpp
@@ -39,6 +39,7 @@ void Piezas::reset()
   for (int i = 0; i < BOARD_ROWS; i++){
     for(int j = 0; j < BOARD_COLS; j++){
       board[i][j] = Blank;
+      cout << i << " " << j << "  ";
     }
   }
 }
